534f3bc2b9ce64a5a169493c69069040aac214a3
kalmai <leozverres@gmail.com>
2021-08-22 22:15:08 -0400

    first commit

---
+++ hello_world.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+	f "fmt" // you can alias imports like this
+	"time"
+)
+
+// i can comment like this
+
+/* or
+like 
+this */
+
+func main(){
+	f.Println("hello world")
+	f.Println(time.Now())
+}
